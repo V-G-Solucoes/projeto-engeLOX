52a160b698f674f5993baa73cfe386fe6f6c7877
Guilherme Nigri <guilhermetnigri@gmail.com>
2021-04-13 21:26:29 -0300

    Adicionei Classe Operacoes com os getters e setters e alguns metodos de validacao; criei os metodos de exibição dos caracteres da calculadora no label de resultado;

---
+++ src/projeto/engelox/OperacoesModel.java
@@ -0,0 +1,56 @@
+package projeto.engelox;
+
+/**
+ *
+ * @author Nigri
+ */
+public class OperacoesModel {
+    private String Operation = "";
+    private String Result = "";
+
+    /**
+     * @return the operacao
+     */
+    public String getOperation() {
+        return Operation;
+    }
+
+    /**
+     * @param operation the Operation to set
+     */
+    public void setOperation(String operation) {
+        Operation += operation;
+    }
+
+    public void deleteLastDigitOperation()
+    {
+        if (Operation.length() > 0) {
+            String newStringOperation = Operation.substring(0,Operation.length() - 1);
+            Operation = newStringOperation;
+        }
+
+    }
+    /**
+     * @return the result
+     */
+    public String getResult() {
+        return Result;
+    }
+
+    /**
+     * @param result the Result to set
+     */
+    public void setResult(String result) {
+        Result = result;
+    }
+    
+    public boolean isSpecialOperator(){
+        String lastDigit = Operation.substring(Operation.length() - 1);
+        
+        if(lastDigit.equals("-") || lastDigit.equals("+") || lastDigit.equals("/")|| lastDigit.equals("*") || lastDigit.equals(".")){
+            return true;
+        }
+        
+        return false;
+    }
+}
